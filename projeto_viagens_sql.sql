133c06ed6ed760581f4480b8e6eda29dd6ab0ffc
castilhoserafim <wesleycserafim@gmail.com>
2026-01-16 11:34:51 -0300

    Initial commit

---
+++ projeto_viagens_sql.sql
@@ -0,0 +1,5 @@
+CREATE TABLE viagens.destinos (
+	id INT PRIMARY KEY,
+	nome VARCHAR(255) NOT NULL UNIQUE COMMENT 'Nome do Destino',
+	descricao VARCHAR(255) NOT NULL COMMENT 'Descrição do Destino'
+);
