2d6b9176e05a93029d2c9e13ccd981a44fce7e3d
castilhoserafim <wesleycserafim@gmail.com>
2026-01-16 11:51:33 -0300

    Update projeto_viagens_sql.sql

--- projeto_viagens_sql.sql
+++ projeto_viagens_sql.sql
@@ -3,3 +3,22 @@ CREATE TABLE viagens.destinos (
 	nome VARCHAR(255) NOT NULL UNIQUE COMMENT 'Nome do Destino',
 	descricao VARCHAR(255) NOT NULL COMMENT 'Descrição do Destino'
 );
+
+CREATE TABLE "usuarios" (
+  "id" int NOT NULL,
+  "nome" varchar(255) NOT NULL COMMENT 'Nome do usuário',
+  "email" varchar(100) NOT NULL COMMENT 'E-mail do usuário',
+  "endereco" varchar(50) NOT NULL COMMENT 'Endereço do usuario',
+  "data_nascimento" date NOT NULL COMMENT 'Date de nascimento do usuário',
+  PRIMARY KEY ("id"),
+  UNIQUE KEY "email" ("email")
+);
+
+CREATE TABLE "reservas" (
+  "id" int NOT NULL COMMENT 'Identificador único da reserva',
+  "id_usuario" int DEFAULT NULL COMMENT 'Referência ao ID do usuário que fez a reserva',
+  "id_destino" int DEFAULT NULL COMMENT 'Referência ao ID do destino da reserva',
+  "data" date DEFAULT NULL COMMENT 'Data da reserva',
+  "status" varchar(255) DEFAULT 'pendente' COMMENT 'Status da reserva (confirmada, pendente, cancelada,  etc.)',
+  PRIMARY KEY ("id")
+);
