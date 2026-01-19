2e34e0d828e683268ec816ca5c65017b539aeca7
castilhoserafim <wesleycserafim@gmail.com>
2026-01-19 17:29:08 -0300

    Update Indices-Dados.sql

--- Indices-Dados.sql
+++ Indices-Dados.sql
@@ -35,14 +35,14 @@ INSERT INTO usuarios (nome, email, data_nascimento, rua) VALUES
 
 
 
-EXPLAIN SELECT * FROM usuarios WHERE nome = "Maria";
+EXPLAIN SELECT * FROM usuarios WHERE nome = 'Maria';
 
 EXPLAIN SELECT * FROM usuarios us
 INNER JOIN reservas rs
 ON us.id = rs.id_usuario
-WHERE nome = "Maria";
+WHERE nome = 'Maria';
 
 
 CREATE INDEX idx_nome ON usuarios (nome);
 
-EXPLAIN SELECT * FROM usuarios WHERE nome = "Maria";
+EXPLAIN SELECT * FROM usuarios WHERE nome = 'Maria';
