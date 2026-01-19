f00173fbf4bdba00b83a40d043dd262b156c5f32
castilhoserafim <wesleycserafim@gmail.com>
2026-01-19 15:21:24 -0300

    Update Inserir Usuários.sql

--- Inserir Usuários.sql
+++ Inserir Usuários.sql
@@ -1,6 +1,9 @@
 # Inserir Usuários
 
+#INSERT INTO usuarios (id, nome, email, data_nascimento, endereco) VALUES 
+#(1, 'Wesley Castilho', 'teste@teste.com', '2005-06-14', 'Gisele Martins, 305 - Bairro Morumbi/SP'),
+#(2, 'Maria Santos', 'maria@example.com', '1985-08-22', 'Rua B, 456, Cidade Y, Estado Z'), 
+#(3, 'Pedro Souza', 'pedro@example.com', '1998-02-10', 'Avenida C, 789, Cidade X, Estado Y');
+
 INSERT INTO usuarios (id, nome, email, data_nascimento, endereco) VALUES 
-(1, 'Wesley Castilho', 'teste@teste.com', '2005-06-14', 'Gisele Martins, 305 - Bairro Morumbi/SP'),
-(2, 'Maria Santos', 'maria@example.com', '1985-08-22', 'Rua B, 456, Cidade Y, Estado Z'), 
-(3, 'Pedro Souza', 'pedro@example.com', '1998-02-10', 'Avenida C, 789, Cidade X, Estado Y');
+(1, 'João Silva', 'joao@example.com', '1990-05-15', 'Rua A, 123, Cidade X, Estado Y');
