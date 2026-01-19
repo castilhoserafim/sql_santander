9772e3ec48d15b993af46e5ba71f3b84d2e8a586
castilhoserafim <wesleycserafim@gmail.com>
2026-01-19 10:16:01 -0300

    update

---
+++ Inserir Destinos.sql
@@ -0,0 +1,3 @@
+# Inserir Destinos
+
+INSERT INTO destinos (id, nome, descricao) VALUES (1, 'Praia da Canoa', 'Praia com Dunas');
---
+++ Inserir Usuários.sql
@@ -0,0 +1,3 @@
+# Inserir Usuários
+
+INSERT INTO usuarios (id, nome, email, data_nascimento, endereco) VALUES (1, 'Wesley Castilho', 'teste@teste.com', '2005-06-14', 'Gisele Martins, 305 - Bairro Morumbi/SP');
