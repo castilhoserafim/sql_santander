3ae3775db090d710c7bf50c28f58f533046aae3b
castilhoserafim <wesleycserafim@gmail.com>
2026-01-19 14:27:15 -0300

    update

---
+++ Delete.sql
@@ -0,0 +1,4 @@
+DELETE FROM destinos 
+WHERE nome = 'Cachoeira do Vale Verde'
+
+select * from destinos
---
+++ Update.sql
@@ -0,0 +1,5 @@
+UPDATE usuarios
+SET id = 4
+WHERE email = 'teste@teste.com'
+
+select * from usuarios
