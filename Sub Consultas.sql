bceca2c37498bfcad05c7b4e43a23f4e844239df
castilhoserafim <wesleycserafim@gmail.com>
2026-01-19 17:17:26 -0300

    Update Sub Consultas.sql

--- Sub Consultas.sql
+++ Sub Consultas.sql
@@ -3,3 +3,6 @@ WHERE id NOT IN (SELECT id_destino FROM reservas);
 
 SELECT * FROM usuarios
 WHERE id NOT IN (SELECT id_usuario FROM reservas);
+
+SELECT nome, (SELECT COUNT(*) from reservas WHERE id_usuario = usuarios.id) AS 
+total_reservas FROM usuarios;
