5b20f8a863595b8489ba9a30ec39dc7fd0dea3fd
castilhoserafim <wesleycserafim@gmail.com>
2026-01-19 14:23:08 -0300

    update

--- Inserir Reservas.sql
+++ Inserir Reservas.sql
@@ -1,6 +1,6 @@
 # Inserir Reservas
 
 INSERT INTO reservas (id, id_usuario, id_destino, data, status) VALUES 
-#(1,1,1, '2026-11-11', 'pendente'),
+(1,1,1, '2026-11-11', 'pendente'),
 (2, 2, 1, '2026-08-05', 'pendente'), 
 (3, 3, 3, '2026-09-20', 'cancelada');
