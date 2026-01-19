23a4ed3fc7084c7f8cae531a79f1a1b9d9fcea04
castilhoserafim <wesleycserafim@gmail.com>
2026-01-19 10:27:19 -0300

    Update Inserir Destinos.sql

--- Inserir Destinos.sql
+++ Inserir Destinos.sql
@@ -1,3 +1,6 @@
 # Inserir Destinos
 
-INSERT INTO destinos (id, nome, descricao) VALUES (1, 'Praia da Canoa', 'Praia com Dunas');
+INSERT INTO destinos (id, nome, descricao) VALUES 
+#(1, 'Praia da Canoa', 'Praia com Dunas'),
+(2, 'Cachoeira do Vale Verde', 'Uma cachoeira exuberante cercada por natureza'), 
+(3, 'Cidade Histórica de Pedra Alta', 'Uma cidade rica em história e arquitetura');
