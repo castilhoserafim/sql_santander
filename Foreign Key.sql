dc5eb3e6876a54c2712f993279b8e371b10757d2
castilhoserafim <wesleycserafim@gmail.com>
2026-01-19 15:09:51 -0300

    Update Foreign Key.sql

--- Foreign Key.sql
+++ Foreign Key.sql
@@ -5,3 +5,8 @@ FOREIGN KEY(id_usuario) REFERENCES usuarios (id);
 ALTER TABLE reservas
 ADD CONSTRAINT fk_reservas_destinos
 FOREIGN KEY(id_destino) REFERENCES destinos (id);
+
+ALTER TABLE reservas
+ADD CONSTRAINT fk_usuarios
+FOREIGN KEY(id_usuario) REFERENCES usuarios (id)
+ON DELETE CASCADE;
